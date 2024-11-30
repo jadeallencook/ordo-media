fa63d377ba746dae2881a47e55bda4432388d7bb
Jade Allen Cook <jadeallencook@gmail.com>
2024-11-29 21:13:11 -0800

    Added folder selector to main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <main className="container"></main>;
+  // path of directory
+  const [path, setPath] = useState<string>();
+  const pathInputRef = (input: HTMLInputElement | null) =>
+    input?.setAttribute("webkitdirectory", "true");
+  const handlePathForm = () => console.log(true);
+
+  return (
+    <main className="container">
+      {!path ? (
+        <form onSubmit={handlePathForm}>
+          <h1>Select Folder To Organize</h1>
+          <input type="file" multiple ref={pathInputRef} />
+          <input type="submit" value="Analyize" />
+        </form>
+      ) : (
+        <div>Organize Media</div>
+      )}
+    </main>
+  );
 }
 
 export default App;
